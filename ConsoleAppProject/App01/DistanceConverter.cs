cfac14b32da406786a7b6a56d7114309a7d46597
AtishAppadu <71987929+AtishAppadu@users.noreply.github.com>
2021-02-08 14:13:45 +0000

    Added Empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,45 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This App is designed to convert distance in miles to the equivalent
+    /// distance in feet
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Atish Appadu version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
 
+        private double feet;
+
+
+        /// <summary>
+        /// Run is for the program to run the changes that has been made and to test whether the program works. 
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// The user will be prompt to input the distance in miles
+        /// Input the miles as double number
+        /// </summary>
+
+        private void InputMiles()
+        {
+
+        }
+
+        private void CalculateFeet()
+        {
+
+        }
+
+        private void OutputFeet()
+        {
+
+        }
     }
 }
